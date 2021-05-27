94437e921d076216b37aeefb325024e040df9a08
matsuhav <28914976+matsuhav@users.noreply.github.com>
2021-05-27 20:46:36 +0900

    Fix completion of redirection Eliminate duplications of completion results Exclude Windows PATH when on WSL or MSYS2

--- autoload/bash.vim
+++ autoload/bash.vim
@@ -1,6 +1,30 @@
-let s:pluginPath = expand('<sfile>:p:h') . '/..'
+let s:completer = expand('<sfile>:p:h') . '/../completer.sh '
+if $WSL_DISTRO_NAME != '' || $MSYSTEM != ''
+	let s:env = slice(substitute($PATH . ':', '\v\/(mnt\/)?c\/[^:]+:', '', 'g'), 0, -1)
+	let s:completer = 'PATH=' . s:env . ' ' . s:completer
+endif
+
 
 function! g:bash#complete(part)
-	let l:spaceend = strcharpart(a:part, strchars(a:part) - 1) == ' '
-	return systemlist(s:pluginPath . '/completer.sh ' . l:spaceend . ' ' . a:part)
+	if a:part == ''
+		return ''
+	endif
+	let l:quote_part = join(map(split(a:part . ' ', ' '), '"''" . v:val . "''"'))
+	let l:filtered = s:filter(systemlist(s:completer . l:quote_part))
+	if split(a:part, ' ')[-1] =~# '^<'
+		let l:filtered = map(l:filtered, '"<" . v:val')
+	endif
+	return l:filtered
+endfunction
+
+function! s:filter(list)
+	let l:set = {}
+	let l:newlist = []
+	for l:index in range(len(a:list))
+		if !has_key(l:set, a:list[l:index])
+			let l:set[a:list[l:index]] = ""
+			let l:newlist += [a:list[l:index]]
+		endif
+	endfor
+	return l:newlist
 endfunction
--- completer.sh
+++ completer.sh
@@ -1,50 +1,36 @@
 #!/usr/bin/env bash
-
 # Original author : Brian Beffa <brbsix@gmail.com>
 # Original version: https://brbsix.github.io/2015/11/29/accessing-tab-completion-programmatically-in-bash/
 #                   https://github.com/rantasub/vim-bash-completion
 
-spaceend=$1
-shift
-
-if [[ $# == 1 && $spaceend -eq 0 ]]; then
-	# complete command
+if [[ $# == 1 ]]; then
+	# Complete command
 	printf '%s\n' $(compgen -acdf $1)
 	exit 0
 fi
 
-# load bash-completion
-completionDir="${BASH_COMPLETION_DIR:-/usr/share/bash-completion}"
-. "${completionDir}/bash_completion"
-
+# Load bash-completion
+. "${BASH_COMPLETION_DIR:-/usr/share/bash-completion}/bash_completion"
 
 COMP_LINE=$*
 COMP_POINT=${#COMP_LINE}
 COMP_WORDS=("$@")
-if [[ $spaceend -eq 1 ]]; then
-	COMP_WORDS+=('')
-fi
-# index of the last word
 COMP_CWORD=$(( ${#COMP_WORDS[@]} - 1 ))
-
-completion=$(complete -p "$1" 2>/dev/null | awk '{print $(NF-1)}')
-
-if [[ -z ${completion} ]]; then
-	# some complicated completions are loaded on demand
-	_completion_loader "$1"
-	completion=$(complete -p "$1" 2>/dev/null | awk '{print $(NF-1)}')
+word_preceding=''
+if [[ ${#COMP_WORDS[@]} -gt 1 ]]; then
+	word_preceding=${COMP_WORDS[-2]}
 fi
 
-# exit when not found
-if [[ -z ${completion} ]]; then
+complete_func=$(complete -p "$1" 2>/dev/null | awk '{print $(NF-1)}')
+if [[ -z ${complete_func} ]]; then
+	# Load on-demand and check again
+	_completion_loader "$1"
+	complete_func=$(complete -p "$1" 2>/dev/null | awk '{print $(NF-1)}')
+fi
+if [[ -z ${complete_func} ]]; then
+	# Exit when not found
 	exit 1
 fi
 
-wordBeforeCompletion=''
-if [[ ${#COMP_WORDS[@]} -gt 1 ]]; then
-	wordBeforeCompletion=${COMP_WORDS[-2]}
-fi
-
-"$completion" "${COMP_WORDS[0]}" "${COMP_WORDS[-1]}" "${wordBeforeCompletion}" 2>/dev/null
-
+"${complete_func}" "${COMP_WORDS[0]}" "${COMP_WORDS[-1]}" "${word_preceding}" 2>/dev/null
 printf '%s\n' "${COMPREPLY[@]}"
